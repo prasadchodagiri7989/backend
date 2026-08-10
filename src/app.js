14111397d0bd5d2d342bb1dc0cbc110176b23bae
SriramMarisetti <srirammarisetti1@gmail.com>
2026-08-10 16:14:09 +0530

    rate limiter commit

--- src/app.js
+++ src/app.js
@@ -75,7 +75,7 @@ app.use(
   '/api',
   rateLimit({
     windowMs: 15 * 60 * 1000,
-    max: 100,
+    max: 10000,
     standardHeaders: true,
     legacyHeaders: false,
     message: { error: 'Too many requests, please try again later.' },
